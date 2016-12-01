c26f013a692051701f002d048be5a1bb9ec3158a
Demivrga <Kaeligos@gmail.com>
2016-12-01 13:59:05 -0500

    - Console commands

--- src/main/java/com/WhosOnline/commands/Online.java
+++ src/main/java/com/WhosOnline/commands/Online.java
@@ -1,5 +1,9 @@
 package com.WhosOnline.commands;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,75 +15,125 @@ public class Online implements CommandExecutor {
 
 	public boolean onCommand(CommandSender cs, Command cmd, String lbl, String[] args) {
 
-		Player p = (Player) cs;
+		int a = Bukkit.getOnlinePlayers().size();
+		List<String> online = new ArrayList<String>();
+
+		for (Player p : Bukkit.getOnlinePlayers()) {
+			online.add(p.getName());
+		}
 
 		if (lbl.equalsIgnoreCase("online")) {
-			if (!(args.length == 1)) {
-				p.openInventory(WhosOnlineMenu.onlinePlayers(p, 1));
 
-			} else {
-				if (isParsable(args[0])) {
-					int x = Integer.parseInt(args[0]);
-					p.openInventory(WhosOnlineMenu.onlinePlayers(p, x));
+			if ((cs instanceof Player)) {
+
+				Player p = (Player) cs;
+
+				if (!(args.length == 1)) {
+					p.openInventory(WhosOnlineMenu.onlinePlayers(p, 1));
+
 				} else {
-					p.sendMessage("ERROR!: Command: /<command> <#>");
+					if (isParsable(args[0])) {
+						int x = Integer.parseInt(args[0]);
+						p.openInventory(WhosOnlineMenu.onlinePlayers(p, x));
+					} else {
+						p.sendMessage("ERROR!: Command: /<command> <#>");
+					}
 				}
+			} else {
+				System.out.println("Online Players: " + a);
+				System.out.println(online);
 			}
 		}
 
 		if (lbl.equalsIgnoreCase("who")) {
-			if (!(args.length == 1)) {
-				p.openInventory(WhosOnlineMenu.onlinePlayers(p, 1));
 
-			} else {
-				if (isParsable(args[0])) {
-					int x = Integer.parseInt(args[0]);
-					p.openInventory(WhosOnlineMenu.onlinePlayers(p, x));
+			if ((cs instanceof Player)) {
+
+				Player p = (Player) cs;
+
+				if (!(args.length == 1)) {
+					p.openInventory(WhosOnlineMenu.onlinePlayers(p, 1));
+
 				} else {
-					p.sendMessage("ERROR!: /<command> <#>");
+					if (isParsable(args[0])) {
+						int x = Integer.parseInt(args[0]);
+						p.openInventory(WhosOnlineMenu.onlinePlayers(p, x));
+					} else {
+						p.sendMessage("ERROR!: Command: /<command> <#>");
+					}
 				}
+			} else {
+				System.out.println("Online Players: " + a);
+				System.out.println(online);
 			}
 		}
 
 		if (lbl.equalsIgnoreCase("list")) {
-			if (!(args.length == 1)) {
-				p.openInventory(WhosOnlineMenu.onlinePlayers(p, 1));
 
-			} else {
-				if (isParsable(args[0])) {
-					int x = Integer.parseInt(args[0]);
-					p.openInventory(WhosOnlineMenu.onlinePlayers(p, x));
+			if ((cs instanceof Player)) {
+
+				Player p = (Player) cs;
+
+				if (!(args.length == 1)) {
+					p.openInventory(WhosOnlineMenu.onlinePlayers(p, 1));
+
 				} else {
-					p.sendMessage("ERROR!: /<command> <#>");
+					if (isParsable(args[0])) {
+						int x = Integer.parseInt(args[0]);
+						p.openInventory(WhosOnlineMenu.onlinePlayers(p, x));
+					} else {
+						p.sendMessage("ERROR!: Command: /<command> <#>");
+					}
 				}
+			} else {
+				System.out.println("Online Players: " + a);
+				System.out.println(online);
 			}
 		}
 
 		if (lbl.equalsIgnoreCase("users")) {
-			if (!(args.length == 1)) {
-				p.openInventory(WhosOnlineMenu.onlinePlayers(p, 1));
 
-			} else {
-				if (isParsable(args[0])) {
-					int x = Integer.parseInt(args[0]);
-					p.openInventory(WhosOnlineMenu.onlinePlayers(p, x));
+			if ((cs instanceof Player)) {
+
+				Player p = (Player) cs;
+
+				if (!(args.length == 1)) {
+					p.openInventory(WhosOnlineMenu.onlinePlayers(p, 1));
+
 				} else {
-					p.sendMessage("ERROR!: /<command> <#>");
+					if (isParsable(args[0])) {
+						int x = Integer.parseInt(args[0]);
+						p.openInventory(WhosOnlineMenu.onlinePlayers(p, x));
+					} else {
+						p.sendMessage("ERROR!: Command: /<command> <#>");
+					}
 				}
+			} else {
+				System.out.println("Online Players: " + a);
+				System.out.println(online);
 			}
 		}
 
 		if (lbl.equalsIgnoreCase("whom")) {
-			if (!(args.length == 1)) {
-				p.openInventory(WhosOnlineMenu.onlinePlayers(p, 1));
 
-			} else {
-				if (isParsable(args[0])) {
-					int x = Integer.parseInt(args[0]);
-					p.openInventory(WhosOnlineMenu.onlinePlayers(p, x));
+			if ((cs instanceof Player)) {
+
+				Player p = (Player) cs;
+
+				if (!(args.length == 1)) {
+					p.openInventory(WhosOnlineMenu.onlinePlayers(p, 1));
+
 				} else {
-					p.sendMessage("ERROR!: /<command> <#>");
+					if (isParsable(args[0])) {
+						int x = Integer.parseInt(args[0]);
+						p.openInventory(WhosOnlineMenu.onlinePlayers(p, x));
+					} else {
+						p.sendMessage("ERROR!: Command: /<command> <#>");
+					}
 				}
+			} else {
+				System.out.println("Online Players: " + a);
+				System.out.println(online);
 			}
 		}
 		return false;
@@ -94,5 +148,4 @@ public class Online implements CommandExecutor {
 		}
 		return parsable;
 	}
-
 }
